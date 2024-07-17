24fd89da7aa870cf087aa5478293b8e9e66fe7da
Vela-Test1993 <rvelu0304@gmail.com>
2024-07-17 22:21:59 +0530

    created new Java Program

---
+++ src/main/java/day1Java/Day1HelloWorld.java
@@ -0,0 +1,11 @@
+package day1Java;
+
+public class Day1HelloWorld {
+	public void welcome() {
+		System.out.println("Hello World");
+	}
+public static void main(String[] args) {
+	Day1HelloWorld d = new Day1HelloWorld();
+	d.welcome();
+}
+}
---
+++ src/main/java/day1Java/TestEmployee.java
@@ -0,0 +1,16 @@
+package day1Java;
+
+public class TestEmployee {
+	public void empName() {
+		System.out.println("Employee Name: Velu R");
+	}
+	public void empId() {
+		System.out.println("Employee Id: Zoo72");
+	}
+	public static void main(String[] args) {
+		TestEmployee test = new TestEmployee();
+		test.empId();
+		test.empName();
+	}
+
+}
